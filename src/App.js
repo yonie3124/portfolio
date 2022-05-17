d8bf0179af5d9b03f68be2fda285a81d25d7a40c
Yonas Kessete Kifle <98472333+yonie3124@users.noreply.github.com>
2022-05-17 10:10:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,12 +20,12 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path='/Profile' element={<ProfilePic />} />
-          <Route className='Home' exact path='/' element={<Home />} />
-          <Route exact path='/Education' element={<Education />} />
-          <Route exact path='/Portfolio' element={<Portfolio />} />
-          <Route exact path='/Experiences' element={<Experience />} />
-          <Route exact path='/Contacts' element={<Contacts />} />
-          <Route exact path='/Email' element={<Email />} />
+          <Route className='Home' exact path='https://yonie3124.github.io/' element={<Home />} />
+          <Route exact path='https://yonie3124.github.io/Education' element={<Education />} />
+          <Route exact path='https://yonie3124.github.io/Portfolio' element={<Portfolio />} />
+          <Route exact path='https://yonie3124.github.io/Experiences' element={<Experience />} />
+          <Route exact path='https://yonie3124.github.io/Contacts' element={<Contacts />} />
+          <Route exact path='https://yonie3124.github.io/Email' element={<Email />} />
         </Routes>
       </div>
       <Footer />
